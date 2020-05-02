c556261a3f335e25f5b1bfe87cbecb60426a9bc7
William Mead <will@Williams-iMac.local>
2020-05-02 11:47:19 +0100

    created robot_boot.py

---
+++ src/robot_boot.py
@@ -0,0 +1,27 @@
+import time
+
+tick = u"\u2713"
+
+on = input("")
+
+print("Beeeeeep")
+time.sleep(1)
+print(f"Booting R.O.B.O.T")
+time.sleep(1)
+print(f"R.O.B.O.T is loading tools")
+
+print(".")
+time.sleep(1)
+print("..")
+time.sleep(1)
+print("...")
+time.sleep(1)
+
+print(f"Starting Systems {tick}")
+time.sleep(1)
+print(f"Booting OS {tick}")
+time.sleep(1)
+print(f"Initiating Robotics {tick}")
+time.sleep(1)
+
+print("R.O.B.O.T is ready for action.")
